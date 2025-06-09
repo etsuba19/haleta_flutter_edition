947ca0d67443743ec93037c03085db1c7aaa1099
Selamawit Kinetibeb <selamawitkinetibeb@gmail.com>
2025-06-09 03:16:09 -0700

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'navigation/app_router.dart';
+import 'package:quiz_admin/presentation/admin_home/admin_home_page.dart';
+import 'package:quiz_admin/presentation/select_difficulty/select_difficulty_page.dart';
+import 'presentation/resources/resources_controller.dart';
+import 'presentation/resources/resources_page.dart';
+import 'application/resources/usecases/resource_usecase_types.dart';
+import 'domain/resources/entities/resource.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        getResourcesProvider.overrideWithValue(() async => List.generate(
+          7,
+              (i) => Resource(
+            id: '${i + 1}',
+            title: 'ResourceID',
+            description: 'Resource ${i + 1}',
+            url: 'https://example.com/${i + 1}',
+          ),
+        )),
+        removeResourceProvider.overrideWithValue((String id) async {}),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-    );
+    return const MaterialApp(home: AdminHomePage());
   }
 }
